a0db26ac1842078ae800d6572f923d7badffe6c8
Eatcoding <trivialcoding6@gmail.com>
2025-03-28 14:45:25 +0900

    로그 설정 추가

--- app/main.py
+++ app/main.py
@@ -1,9 +1,11 @@
 from fastapi import FastAPI
+from app.core.config import settings
 
 app = FastAPI(
     title="9git-backend API",
     description="9git-backend API",
     version="0.1.0",
+    debug=settings.DB_ECHO_LOG,
 )
 
 
