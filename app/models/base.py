72b87dde39ee65b1367afc564c49079da6b5657b
gideok Kim <ikid4127@naver.com>
2025-04-08 11:58:25 +0900

    🐛 누락된 `metadata` 코드 추가
    `metadata`는 DB 스키마 정보를 담는 컨테이너이므로 없으면 안됨.

--- app/models/base.py
+++ app/models/base.py
@@ -1,5 +1,5 @@
 # `func`을 추가하여 SQL 기본 함수 활용
-from sqlalchemy import DateTime, func
+from sqlalchemy import DateTime, func, MetaData
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 from sqlalchemy.dialects.postgresql import UUID
 from uuid import uuid4
@@ -15,6 +15,8 @@ class Base(DeclarativeBase):
     """Base class which provides automated table name
     and surrogate primary key column.
     """
+
+    metadata = MetaData()
 
     id: Mapped[UUID] = mapped_column(
         UUID(as_uuid=True), primary_key=True, default=uuid4
