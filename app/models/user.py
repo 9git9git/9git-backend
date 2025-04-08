28831ad9d448d15fb49838f8819a883384c16322
Eatcoding <trivialcoding6@gmail.com>
2025-04-08 09:23:54 +0900

    🩹 User Table sex 컬럼 필수값 제외

--- app/models/user.py
+++ app/models/user.py
@@ -22,9 +22,7 @@ class User(Base):
     email: Mapped[str] = mapped_column(String(255), unique=True, nullable=False)
     name: Mapped[str] = mapped_column(String(100), nullable=False)
     password: Mapped[str] = mapped_column(String(255), nullable=False)
-    sex: Mapped[GenderEnum] = mapped_column(
-        Enum(GenderEnum, name="gender_enum"), nullable=False
-    )
+    sex: Mapped[GenderEnum] = mapped_column(Enum(GenderEnum, name="gender_enum"))
     age: Mapped[int] = mapped_column(Integer)
     job: Mapped[str] = mapped_column(String(100))
     level: Mapped[int] = mapped_column(Integer, default=1)
