e140dbf1fedb08957e60534d90ddb671ae93e0e5
Eatcoding <trivialcoding6@gmail.com>
2025-04-13 13:08:27 +0900

    ✨ Jwt 시크릿 키 및 쿠키 secure 옵션 환경 변수 추가
    secret key는 "헌찬" 한테 요청 주시면 문자열 드립니닷 그리고 secure 옵션은 https 에서만 할건지에 대한 옵션인데 개발환경에선 사용하지 않으므로 환경변수로 동적으로 적용되게 했습니다

--- app/core/config.py
+++ app/core/config.py
@@ -9,6 +9,10 @@ class Settings(BaseSettings):
     ASYNC_DATABASE_URL: str
     # 동기 데이터베이스 연결 문자열 (Alembic 에서 사용)
     SYNC_DATABASE_URL: str
+    # 서버 시크릿 키
+    SECRET_KEY: str
+    # 쿠키 보안 설정
+    IS_SECURE: bool
 
     model_config = ConfigDict(env_file=".env", env_file_encoding='utf-8', extra="allow")
 
