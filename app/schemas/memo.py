5b3fed1898fed67eb0152cc351e9ae62e3510290
gideok Kim <ikid4127@naver.com>
2025-04-13 13:24:19 +0900

    ✨ Memo Schema 추가

---
+++ app/schemas/memo.py
@@ -0,0 +1,28 @@
+from app.schemas.base import BaseModel
+from uuid import UUID
+from datetime import datetime
+
+
+class MemoCreate(BaseModel):
+    userId: UUID
+    categoryId: UUID
+    title: str
+    content: str
+    startDate: datetime
+    endDate: datetime
+
+
+class MemoResponse(BaseModel):
+    id: UUID
+    category_id: UUID
+    title: str
+    content: str
+    start_date: datetime
+    end_date: datetime
+
+
+class MemoUpdate(BaseModel):
+    title: str
+    content: str
+    startDate: datetime
+    endDate: datetime
