863d66a32b823f9c0dabf65fb5ef5815166f509e
Eatcoding <trivialcoding6@gmail.com>
2025-04-13 15:47:55 +0900

    🩹 서비스 에러 로직은 service 파일에 구현

--- app/services/user.py
+++ app/services/user.py
@@ -1,3 +1,4 @@
+from fastapi import HTTPException
 from sqlalchemy.ext.asyncio import AsyncSession
 from app.crud.user import (
     read_users,
@@ -10,10 +11,16 @@ from app.crud.user import (
 from app.schemas.user import UserCreate, UserResponse, UserUpdate
 from typing import List
 from uuid import UUID
+from fastapi import status
 
 
 async def select_user_by_id(db: AsyncSession, user_id: UUID) -> UserResponse:
-    return await read_user_by_id(db, user_id)
+    user = await read_user_by_id(db, user_id)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="사용자를 찾을 수 없습니다."
+        )
+    return user
 
 
 async def select_user_by_email(db: AsyncSession, email: str) -> UserResponse:
@@ -25,12 +32,23 @@ async def select_users(db: AsyncSession) -> List[UserResponse]:
 
 
 async def register_user(db: AsyncSession, user_data: UserCreate) -> UserResponse:
+    db_user = await read_user_by_email(db, user_data.email)
+    if db_user:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="이미 존재하는 이메일입니다.",
+        )
     return await create_user(db, user_data)
 
 
 async def update_user_by_id(
     db: AsyncSession, user_id: UUID, user_data: UserUpdate
 ) -> UserResponse:
+    db_user = await read_user_by_id(db, user_id)
+    if not db_user:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="사용자를 찾을 수 없습니다."
+        )
     return await update_user(db, user_id, user_data)
 
 
