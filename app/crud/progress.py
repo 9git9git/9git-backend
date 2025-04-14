1633d94e8d20fe116c97f9256d116c727e46665d
Ryan-OH <dongjune0971@gmail.com>
2025-04-14 10:39:07 +0900

    🩹 ProgressResponse로 변경

--- app/crud/progress.py
+++ app/crud/progress.py
@@ -1,13 +1,14 @@
+from typing import List, Optional
 from sqlalchemy import select, delete
 from sqlalchemy.ext.asyncio import AsyncSession
 from uuid import UUID
-from typing import List, Optional
-
 from app.models.category import Progress
 from app.schemas.progress import ProgressCreate, ProgressUpdate, ProgressResponse
 
 
-async def create_progress(db: AsyncSession, progress_data: ProgressCreate) -> Progress:
+async def create_progress(
+    db: AsyncSession, progress_data: ProgressCreate
+) -> ProgressResponse:
     progress = Progress(
         user_id=progress_data.user_id,
         category_id=progress_data.category_id,
@@ -17,33 +18,42 @@ async def create_progress(db: AsyncSession, progress_data: ProgressCreate) -> Pr
     db.add(progress)
     await db.commit()
     await db.refresh(progress)
-    return progress
+    return ProgressResponse.model_validate(progress)
 
 
-async def read_all_progresses(db: AsyncSession) -> List[Progress]:
+async def read_all_progresses(db: AsyncSession) -> List[ProgressResponse]:
     result = await db.execute(select(Progress))
-    return result.scalars().all()
+    progresses = result.scalars().all()
+    return [ProgressResponse.model_validate(p) for p in progresses]
 
 
 async def read_progress_by_id(
     db: AsyncSession, progress_id: UUID
-) -> Optional[Progress]:
+) -> Optional[ProgressResponse]:
     result = await db.execute(select(Progress).where(Progress.id == progress_id))
-    return result.scalars().first()
+    progress = result.scalars().first()
+    return ProgressResponse.model_validate(progress) if progress else None
 
 
 async def update_progress(
     db: AsyncSession, progress: Progress, progress_data: ProgressUpdate
-) -> Progress:
+) -> ProgressResponse:
     update_data = progress_data.model_dump(exclude_unset=True)
     for key, value in update_data.items():
         setattr(progress, key, value)
     await db.commit()
     await db.refresh(progress)
-    return progress
+    return ProgressResponse.model_validate(progress)
 
 
 async def delete_progress(db: AsyncSession, progress: Progress) -> bool:
-    await db.execute(delete(Progress).where(Progress.id == progress.id))
-    await db.commit()
+    delete_statement = delete(Progress).where(Progress.id == progress.id)
+
+    try:
+        await db.execute(delete_statement)
+        await db.commit()
+    except Exception as e:
+        await db.rollback()
+        raise e
+
     return True
