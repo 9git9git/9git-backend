08487bfa4a53f3ef9b069188250463331afc1aac
Ryan-OH <dongjune0971@gmail.com>
2025-04-14 10:38:43 +0900

    🩹 Try - Exeption 사용

--- app/services/progress.py
+++ app/services/progress.py
@@ -49,11 +49,19 @@ async def update_progress_by_id(
     return updated
 
 
-async def delete_progress_by_id(db: AsyncSession, progress_id: UUID) -> bool:
-    progress = await read_progress_by_id(db, progress_id)
-    if not progress:
+async def delete_progress_service(db: AsyncSession, progress_id: UUID) -> bool:
+    try:
+        progress = await read_progress_by_id(db, progress_id)
+        if not progress:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail="진행률을 찾을 수 없습니다.",
+            )
+
+        return await delete_progress(db, progress)
+
+    except Exception as e:
         raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail="진행률을 찾을 수 없습니다.",
-        )
-    return await delete_progress(db, progress)
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="데이터베이스 오류가 발생했습니다.",
+        ) from e
